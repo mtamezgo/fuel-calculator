f0d0df2b32d8d9a113cb66810b0f77884dc1dfe6
mtamezgo <mtamezgo@gmail.com>
2025-12-15 16:49:19 -0800

    Simplify Vercel handler and ensure build files are included

--- api/index.js
+++ api/index.js
@@ -1,35 +1,7 @@
-// This is a Vercel Serverless Function
-export default async function handler(req, res) {
-  // Dynamically import the build
-  const { createRequestHandler } = await import("@react-router/node");
-  const build = await import("../build/server/index.js");
+import { createRequestHandler } from "@react-router/node";
+import * as build from "../build/server/index.js";
 
-  const requestHandler = createRequestHandler({ build });
+// Create the request handler once
+const handler = createRequestHandler({ build });
 
-  // Convert Vercel request to standard Request
-  const url = new URL(req.url || '/', `https://${req.headers.host}`);
-  const request = new Request(url, {
-    method: req.method,
-    headers: new Headers(req.headers),
-    body: req.method !== 'GET' && req.method !== 'HEAD' ? JSON.stringify(req.body) : undefined,
-  });
-
-  try {
-    const response = await requestHandler(request);
-
-    // Set status code
-    res.status(response.status);
-
-    // Set headers
-    response.headers.forEach((value, key) => {
-      res.setHeader(key, value);
-    });
-
-    // Send body
-    const body = await response.text();
-    res.send(body);
-  } catch (error) {
-    console.error('Handler error:', error);
-    res.status(500).json({ error: 'Internal Server Error', message: error.message });
-  }
-}
+export default handler;

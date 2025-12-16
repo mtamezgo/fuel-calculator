e87b5ae3fa18432f61df808ee641f4ea8e038f50
mtamezgo <mtamezgo@gmail.com>
2025-12-15 16:42:15 -0800

    Fix Vercel serverless function routing

--- server.js
+++
@@ -1,8 +0,0 @@
-import { createRequestHandler } from "@react-router/node";
-import * as build from "./build/server/index.js";
-
-const handler = createRequestHandler({ build });
-
-export default async function (req, res) {
-  return handler(req, res);
-}
